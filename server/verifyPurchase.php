5a71c2478581c5d03191f02ce72caafbab22221b
Tubebaum <tubebaum@gmail.com>
2012-08-15 16:11:31 -0400

    Optimized purchase verification

--- server/verifyPurchase.php
+++ server/verifyPurchase.php
@@ -13,22 +13,26 @@ $ks = $client->session->start(ADMIN_SECRET, $USER_ID, KalturaSessionType::ADMIN,
 $client->setKs($ks);
 
 $response = 'false';
-$filter = new KalturaMetadataFilter();
-$filter->metadataProfileIdEqual = PAYPAL_USER_METADATA_PROFILE_ID;
-$filter->metadataObjectTypeEqual = 3;
-$filter->objectIdEqual = $USER_ID;
+$filter = new KalturaUserFilter();
+$filter->idEqual = $USER_ID;
 $pager = new KalturaFilterPager();
 $pager->pageSize = 1;
 $pager->pageIndex = 1;
-$results = $client->metadata->listAction($filter, $pager)->objects;
+$filterAdvancedSearch = new KalturaMetadataSearchItem();
+$filterAdvancedSearch->type = KalturaSearchOperatorType::SEARCH_AND;
+$filterAdvancedSearch->metadataProfileId = PAYPAL_USER_METADATA_PROFILE_ID;
+$filterAdvancedSearchItems = array();
+$filterAdvancedSearchItems0 = new KalturaSearchCondition();
+if(is_numeric($_REQUEST['id']))
+	$filterAdvancedSearchItems0->field = "/*[local-name()='metadata']/*[local-name()='PurchasedCategories']";
+else
+	$filterAdvancedSearchItems0->field = "/*[local-name()='metadata']/*[local-name()='PurchasedEntries']";
+$filterAdvancedSearchItems0->value = $_REQUEST['id'];
+$filterAdvancedSearchItems[0] = $filterAdvancedSearchItems0;
+$filterAdvancedSearch->items = $filterAdvancedSearchItems;
+$filter->advancedSearch = $filterAdvancedSearch;
+$results = $client->user->listAction($filter, $pager)->objects;
 if(count($results) > 0) {
-	$result = $results[0];
-	$xml = simplexml_load_string($result->xml);
-	foreach($xml as $key => $value) {
-		if($_REQUEST['id'] == $value) {
-			$response = 'true';
-			break;
-		}
-	}
+	$response = 'true';
 }
 echo $response;
